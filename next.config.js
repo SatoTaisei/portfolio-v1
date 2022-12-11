c6d6016ab46a6cf3894dd9cdde876443eb75b2b9
Sato Taisei <t.sei43rm@gmail.com>
2022-12-11 18:15:00 +0900

    🎉 Begin a project!!

---
+++ next.config.js
@@ -0,0 +1,7 @@
+/** @type {import('next').NextConfig} */
+
+module.exports = {
+    reactStrictMode: true,
+    swcMinify: true,
+    pageExtensions: ['page.tsx', 'page.ts', 'page.jsx', 'page.js'],
+};
