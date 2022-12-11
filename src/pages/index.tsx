c8bb74ce076a4ae1771faf48beb6973ee786e0d3
Sato Taisei <t.sei43rm@gmail.com>
2022-12-11 18:38:33 +0900

    🔥 code in next.config

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,6 @@
 import Head from 'next/head';
 
-import { Footer } from '@/components/Footer/Footer';
+import { Footer } from '@/components/Footer';
 import styles from '@styles/Home.module.scss';
 
 export default function Home() {
