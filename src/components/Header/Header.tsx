247d7463db56fbf5222846703f1cf4a5a4888a31
Sato Taisei <t.sei43rm@gmail.com>
2023-01-01 17:05:36 +0900

    ♻️ create Menu List

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -2,32 +2,26 @@ import Link from 'next/link';
 
 import styles from './Header.module.scss';
 
+const MENU_LIST = [
+    { name: 'showcase', url: '#' },
+    { name: 'timeline', url: '#' },
+    { name: 'contact', url: '#' },
+    { name: 'profile', url: '#' },
+];
+
 export const Header = () => {
     return (
         <header className={styles.wrapper}>
             <span className={styles.logo}>Sato Taisei</span>
             <div className={styles.menu}>
                 <ul className={styles.list}>
-                    <li className={styles.item}>
-                        <Link href={'#'} className={styles.link}>
-                            showcase
-                        </Link>
-                    </li>
-                    <li className={styles.item}>
-                        <Link href={'#'} className={styles.link}>
-                            timeline
-                        </Link>
-                    </li>
-                    <li className={styles.item}>
-                        <Link href={'#'} className={styles.link}>
-                            contact
-                        </Link>
-                    </li>
-                    <li className={styles.item}>
-                        <Link href={'#'} className={styles.link}>
-                            profile
-                        </Link>
-                    </li>
+                    {MENU_LIST.map((item, index) => (
+                        <li className={styles.item} key={index}>
+                            <Link href={item.url} className={styles.link}>
+                                {item.name}
+                            </Link>
+                        </li>
+                    ))}
                 </ul>
             </div>
         </header>
