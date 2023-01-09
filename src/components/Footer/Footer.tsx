55718936827abe94f5aff99ccc4b69d1a131e9f2
Sato Taisei <t.sei43rm@gmail.com>
2023-01-09 16:28:55 +0900

    📄 copylight 2022 -> 2023

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -3,7 +3,7 @@ import styles from './Footer.module.scss';
 export const Footer = () => {
     return (
         <footer className={styles.wrapper}>
-            <span className={styles.copylight}>&copy; Sato Taisei 2022</span>
+            <span className={styles.copylight}>&copy; Sato Taisei 2023</span>
         </footer>
     );
 };
