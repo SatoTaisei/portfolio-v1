8113ac54ec725ff76b40e851a8896f7b20db1bc2
Sato Taisei <t.sei43rm@gmail.com>
2023-01-09 16:39:47 +0900

    ♻️ ShowCaseItem作成

---
+++ src/components/ShowCase/ShowCaseItem/index.ts
@@ -0,0 +1,1 @@
+export { ShowCaseItem } from './ShowCaseItem';
--- src/components/ShowCase/ShowCaseList/ShowCaseList.tsx
+++ src/components/ShowCase/ShowCaseList/ShowCaseList.tsx
@@ -1,5 +1,7 @@
 import { FC } from 'react';
 
+import { ShowCaseItem } from '@/components/ShowCase/ShowCaseItem';
+
 import styles from './ShowCaseList.module.scss';
 
 type Props = {
@@ -9,7 +11,7 @@ type Props = {
 export const ShowCaseList: FC<Props> = () => {
     return (
         <div className={styles.container}>
-            <div className={styles.box}>Showcase</div>
+            <ShowCaseItem />
         </div>
     );
 };
